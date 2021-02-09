acf8206ac11af2d695f04182cff33217496ad5fd
kswelder <welderhayne2000@gmail.com>
2021-02-09 07:45:50 -0300

    Created succesfull http request get and post

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,29 @@
 const route = require('express').Router()
+const database = require('../database')
 
-route.get('/user', (req, res) => {})
+route.get('/user',async (req, res) => {
+    const { username } = req.body
 
-route.post('/user', (req, res) => {})
+    let response = ""
+
+    await database.select(["username","password"]).where({username}).table('user').then(data => {
+        response = data[0]
+    }).catch(err => {
+        console.log(err)
+    })
+
+    res.json({message: response})
+})
+
+route.post('/user', (req, res) => {
+    const {username,password} = req.body
+
+    database('user').insert({username,password}).then().catch( err => {
+        console.log(err)
+    })
+
+    res.json({username,password})
+})
 
 route.put('/user', (req, res) => {})
 
