f06a8219573542511e4ef186e42f8f0d90df67d4
kswelder <welderhayne2000@gmail.com>
2021-02-09 00:02:48 -0300

    Created user controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,11 @@
+const route = require('express').Router()
+
+route.get('/user', (req, res) => {})
+
+route.post('/user', (req, res) => {})
+
+route.put('/user', (req, res) => {})
+
+route.delete('/user', (req, res) => {})
+
+module.exports = route
