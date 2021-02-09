132dabdcdcaeb4a7a42b5983a506f8f5cfd26ea9
kswelder <welderhayne2000@gmail.com>
2021-02-09 13:13:58 -0300

    Typo removal

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,4 @@
 const routes = require('express').Router()
-const route = require('./controllers/userController')
-const routes = require('./controllers/userController')
 const userController = require('./controllers/userController')
 
 routes.use(userController)
